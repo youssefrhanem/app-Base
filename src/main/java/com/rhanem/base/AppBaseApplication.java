ba47e0f348e75da87b13c1918aaf6f3ae9a3eb49
youssef rhanem <youssefrhanem91@gmail.com>
2024-03-03 21:24:26 +0100

    Initial commit

---
+++ src/main/java/com/rhanem/base/AppBaseApplication.java
@@ -0,0 +1,13 @@
+package com.rhanem.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhanem/base/AppBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhanem.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
